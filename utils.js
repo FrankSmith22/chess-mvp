aa4877cb212df1bce828044cbbd77392ec35d28f
Frank Smith <fasvi6@gmail.com>
2024-08-12 21:33:58 -0400

    Visualize board properly nowe

--- utils.js
+++ utils.js
@@ -1,4 +1,8 @@
+import { Color, Pawn, Rook, Knight, Bishop, King, Queen } from "./pieces.js"
+
+let boardContainer = document.querySelector('.chess-board')
 export function printBoard(frozenBoard){
+    // Print to console
     // frozenBoard = JSON.parse(JSON.stringify(frozenBoard))
     let printStr = "  0        1        2        3        4        5        6        7\n"
     for(let i=0; i < frozenBoard.length; i++){
@@ -14,4 +18,30 @@ export function printBoard(frozenBoard){
         printStr += "\n"
     }
     console.log(printStr)
+
+    // Update view
+    boardContainer.innerHTML = ''
+    let colColor = 'tan'
+    for(let row of frozenBoard){
+        let newRow = document.createElement('div')
+        newRow.className = 'row'
+        for(let col of row){
+            let newCol = document.createElement('div')
+            newCol.className = 'col'
+            newCol.style.backgroundColor = colColor
+            // Toggle next colColor
+            colColor = colColor === 'tan' ? 'brown' : 'tan'
+            if(typeof col === 'string'){
+                newRow.appendChild(newCol)
+                continue
+            }
+            let imageSrc = `piece_pngs/${col.color}_${col.constructor.name.toLowerCase()}.png`
+            let imageElement = document.createElement('img')
+            imageElement.src = imageSrc
+            newCol.appendChild(imageElement)
+            newRow.appendChild(newCol)
+        }
+        colColor = colColor === 'tan' ? 'brown' : 'tan'
+        boardContainer.appendChild(newRow)
+    }
 }
